6a020a98a13ead4e8186504abca1b9b6076b4577
BOSSKEK3 <228652459+BOSSKEK3@users.noreply.github.com>
2025-11-25 09:51:05 +0200

    database functions

--- airguardian_practice_tool/app/pages/api/route.ts
+++ airguardian_practice_tool/app/pages/api/route.ts
@@ -4,8 +4,13 @@ import { forward } from "mgrs";
 import { db } from '@/lib/db/db';
 import { Aircrafttable } from '@/lib/db/schema';
 import { SelectAircraft, InsertAircraft } from '@/lib/db/schema';
-
+import { eq } from 'drizzle-orm';
  
+export interface latlong{
+    lat: number;
+    lng: number;
+}
+
 type ResponseData = {
     id: number;
     aircraftId: string;
@@ -39,11 +44,12 @@ export async function GET(request: Request) {
   const database = db 
   const oldcraft =  await database.select().from(Aircrafttable)
 
-  const newcraft = oldcraft.map(craft => {
-    const latlong = new LatLng(craft.latitude, craft.longitude)
+  const newcraft = oldcraft.map((craft) => {
+    const latlong: latlong = {lat: craft.latitude, lng: craft.longitude}
     let waypointi = craft.waypointindex
-    let waypoint = JSON.parse(craft.waypoints[waypointi])
-    let nexpos = new LatLng(waypoint.latitude, waypoint.longitude)
+    let waypoints = JSON.parse(craft.waypoints)
+    let waypoint = waypoints[waypointi]
+    let nexpos: latlong = {lat: waypoint.latitude, lng: waypoint.longitude}
     const checkwaypoint = checkfornewwaypoint(latlong, nexpos)
     
     if(checkwaypoint){
@@ -54,52 +60,114 @@ export async function GET(request: Request) {
             return {...craft}
         }
         waypointi += 1
-        waypoint = JSON.parse(craft.waypoints[waypointi])
-        nexpos = new LatLng(waypoint.lat, waypoint.lng)
-    
+        waypoints = JSON.parse(craft.waypoints)
+        waypoint = waypoints[waypointi]
+        nexpos = {lat: waypoint.latitude, lng: waypoint.longitude}
+    }
     const h = (360 + calculateheading(latlong, nexpos)) % 360
     const pos = calculateposition(latlong, craft.speed, h)
     craft.latitude = pos.lat
     craft.longitude = pos.lng
     craft.heading = h
+
+
     return {
         ...craft,         
         latitude: pos.lat,
         longitude: pos.lng,
         heading: h,
-        waypointindex: waypointi
+        waypointindex: waypointi,
+        waypoints: JSON.parse(craft.waypoints)
     }
-  }})
+  })
+  newcraft.forEach(async(craft) => {
+    await database.update(Aircrafttable).set({
+    latitude: craft.latitude,
+    longitude: craft.longitude,
+    heading: craft.heading,
+    waypointindex: craft.waypointindex,
+    waypoints: JSON.stringify(craft.waypoints)}).where(eq(Aircrafttable.id, craft.id))
+  });
+
    return NextResponse.json(newcraft)
+}
+
+export async function DELETE(request: Request){
+    const database = db 
+    const reqdata = await request.json()
+    await database.delete(Aircrafttable).where(eq(Aircrafttable.id, reqdata))
+    return NextResponse.json(reqdata)
 }
 
 function handleDeleteAircraft(name: string){
 
 }
 
-  const checkfornewwaypoint = (pos: LatLng, wpos: LatLng) =>{
-    if(GeometryUtil.length([pos, wpos]) <= 300){
+  const checkfornewwaypoint = (pos: latlong, wpos: latlong) =>{
+    if(dist(pos, wpos) <= 300){
         return true
     }
     return false
   }
 
-const calculateposition = (pos: LatLng, speed: number, heading: number) =>{
+const calculateposition = (pos: latlong, speed: number, heading: number) =>{
     const dstchange = speed *  0.00027777777777777778 //one second interval
-    const result = GeometryUtil.destination(pos, heading, dstchange*1000);
+    const result = newpos(pos, heading, dstchange*1000);
     return result
   }
 
-const calculateheading = (curpos: LatLng, nextpos: LatLng) => {
-    /*
-    const p1 = map?.latLngToContainerPoint(curpos)
-    const p2 = map?.latLngToContainerPoint(nextpos)
-    */
-   //TODO: why does the plane fly off the track?????
-    const p1 = new Point(curpos.lat, curpos.lng)
-    const p2 = new Point(nextpos.lat, nextpos.lng)
-    return GeometryUtil.computeAngle(p1!, p2!)
+  function newpos(pos: latlong, heading: number, distance: number){
+
+        heading = (heading + 360) % 360;
+        let rad = Math.PI / 180
+        let radInv = 180 / Math.PI
+        let R = 6378137 // approximation of Earth's radius
+        let lon1 = pos.lng * rad
+        let lat1 = pos.lat * rad
+        let rheading = heading * rad
+        let sinLat1 = Math.sin(lat1)
+        let cosLat1 = Math.cos(lat1)
+        let cosDistR = Math.cos(distance / R)
+        let sinDistR = Math.sin(distance / R)
+        let lat2 = Math.asin(sinLat1 * cosDistR + cosLat1 * sinDistR * Math.cos(rheading))
+        let lon2 = lon1 + Math.atan2(Math.sin(rheading) * sinDistR * cosLat1, cosDistR - sinLat1 * Math.sin(lat2));
+        lon2 = lon2 * radInv;
+        lon2 = lon2 > 180 ? lon2 - 360 : lon2 < -180 ? lon2 + 360 : lon2;
+        return {lat: lat2 * radInv, lng: lon2}
   }
+
+const calculateheading = (curpos: latlong, nextpos: latlong) => {
+    const φ1 = curpos.lat * Math.PI/180;
+    const φ2 = nextpos.lat * Math.PI/180;
+    const Δλ = (nextpos.lng - curpos.lng) * Math.PI/180;
+
+    const y = Math.sin(Δλ) * Math.cos(φ2);
+    const x = Math.cos(φ1) * Math.sin(φ2) -
+              Math.sin(φ1) * Math.cos(φ2) * Math.cos(Δλ);
+
+    let res = Math.atan2(y, x);
+    res = res * 180 / Math.PI;
+
+    return (res + 360) % 360; // normalize 0-360°
+  }
+
+  function dist(pos: latlong, wpos: latlong) {
+    var R = 6371; // Radius of the earth in km
+    var dLat = deg2rad(wpos.lat-pos.lat);  // deg2rad below
+    var dLon = deg2rad(wpos.lng-pos.lng); 
+    var a = 
+        Math.sin(dLat/2) * Math.sin(dLat/2) +
+        Math.cos(deg2rad(pos.lat)) * Math.cos(deg2rad(wpos.lat)) * 
+        Math.sin(dLon/2) * Math.sin(dLon/2)
+        ; 
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+    var d = R * c; // Distance in km    return d
+    return d*1000
+  }
+
+function deg2rad(deg: number) {
+  return deg * (Math.PI/180)
+}
 
 /*
 function mapData(oldcraft: Aircraft): InsertAircraft{
